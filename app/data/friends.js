e789a21f267437a0ffc8496695756070fec9c2dd
Stefanos Bezanis <stefbezanis@gmail.com>
2017-05-05 18:29:28 -0500

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+{
+  name :"Bear",
+  photoURL : "https://upload.wikimedia.org/wikipedia/commons/7/71/2010-kodiak-bear-1.jpg",
+  scores :[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  name :"Another Bear",
+  photoURL :"http://www.wikiality.com/file/2016/11/bears1.jpg",
+  scores :[
+      3,
+      5,
+      2,
+      4,
+      2,
+      1,
+      5,
+      5,
+      3,
+      3
+    ]
+},
+
+
+]
+
+module.exports = friends;
